ad442ddf5bba088e8a5efe81dea0951f9b62f519
emad566 <emade09@gmail.com>
2023-09-15 04:15:23 +0300

    FMs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
     </form>
 
     <!-- Insert this script at the bottom of the HTML, but before you use any Firebase services -->
-    <script src="https://www.gstatic.com/firebasejs/6.0.2/firebase.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/10.4.0/firebase-recaptcha.js"></script>
     <script type="module">
         import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
 
